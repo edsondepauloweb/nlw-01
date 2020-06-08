34b0d57604f96346ff0ced8e3e93d3f49314ce15
Edson de Paulo Ramalho <edsondepauloweb@gmail.com>
2020-06-07 22:14:12 -0300

    Next Level Week 1.0 - Final

---
+++ backend/knexfile.ts
@@ -0,0 +1,19 @@
+// import path from 'path';
+const path = require('path');
+
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'nlw_01',
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+    },
+    useNullAsDefault: true,
+};
---
+++ backend/src/controllers/ItemsController.ts
@@ -0,0 +1,20 @@
+import knex from '../database/connection';
+import { Request, Response } from 'express';
+
+class ItemsController {
+    async index(request: Request, response: Response) {
+        const items = await knex('items').select('*');
+
+        const serializedItems = items.map((item) => {
+            return {
+                id: item.id,
+                title: item.title,
+                image_url: `${process.env.API_URL}/uploads/${item.image}`, // Your LAN ip.
+            };
+        });
+
+        return response.json(serializedItems);
+    }
+}
+
+export default ItemsController;
